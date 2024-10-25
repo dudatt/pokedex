e607a270dd6831b7000087fcc4080d2713084b88
julia-tatim <juliatatimcastro@gmail.com>
2024-10-25 19:53:13 -0300

    Tela de login e cadastro com botão

---
+++ app/src/main/java/com/example/pokedex/Login.kt
@@ -0,0 +1,28 @@
+package com.example.pokedex
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<Button>(R.id.singupbtn).setOnClickListener {
+            val intent = Intent(this, Singup::class.java)
+            startActivity(intent)
+        }
+    }
+}
