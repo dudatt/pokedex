d3db6f25e8f3a4fb76d61c72d2bdbc4674330f7b
julia-tatim <juliatatimcastro@gmail.com>
2024-10-30 16:18:07 -0300

    Layout Main

--- app/src/main/java/com/example/pokedex/Profile.kt
+++ app/src/main/java/com/example/pokedex/Profile.kt
@@ -1,15 +1,30 @@
 package com.example.pokedex
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.pokedex.databinding.ActivityProfileBinding
 
-class profile : AppCompatActivity() {
+class Profile : AppCompatActivity() {
+    lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
+        binding.backIcon.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.back.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
         setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
