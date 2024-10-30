b680100a0258dd3a9d7600ccb62315bb43472044
dudatt <dudamelobarbosa@gmail.com>
2024-10-30 18:51:24 -0300

    drawable renomeado e ajustes no main

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -1,31 +1,43 @@
 package com.example.pokedex
 
+import PokedexViewModel
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.pokedex.databinding.ActivityMainBinding
 import com.example.pokedex.recyclerview.PokemonListAdapter
 
 class MainActivity : ComponentActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: PokedexViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[PokedexViewModel::class.java]
+
+        viewModel.getPokemon()
+
+        viewModel.pokemon.observe(this@MainActivity) { pokedex ->
+            pokedex.forEach { pokemon ->
+                Log.i("POKEMON", "${pokemon.name}")
+
+            }
+        }
 
         //binding.rvPokemon.adapter = PokemonListAdapter()
         binding.rvPokemon.layoutManager = GridLayoutManager(this, 2)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
             val systemBars = insets.getInsets(
@@ -34,15 +46,15 @@ class MainActivity : ComponentActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val adapter = PokemonListAdapter(
-            context = this,
-            pokemon = List(20) {
-                Pokemon("@drawable/charmander", "001", "Charmander", "Fire", "Fire")
-                Pokemon("@drawable/charizard", "002", "Charizard", "Fire", "Fire")
-                Pokemon("@drawable/squirtle", "003", "Squirtle", "Water", "Water")
-                Pokemon("@drawable/wartortle", "004", "Wartortle", "Water", "Water")
-            }
-        )
-        binding.rvPokemon.adapter = adapter
+        //val adapter = PokemonListAdapter(
+        //    context = this,
+        //    pokemon = List(20) {
+        //        Pokemon("@drawable/charmander", "001", "Charmander", "Fire", "Fire")
+        //        Pokemon("@drawable/charizard", "002", "Charizard", "Fire", "Fire")
+        //        Pokemon("@drawable/squirtle", "003", "Squirtle", "Water", "Water")
+        //        Pokemon("@drawable/wartortle", "004", "Wartortle", "Water", "Water")
+        //    }
+        //)
+        //binding.rvPokemon.adapter = adapter
     }
 }
